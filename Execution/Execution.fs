8844b58229453200f4086e0450672a82326430b8
Nick <nikita.karandej@gmail.com>
2017-03-02 22:19:51 +0000

    Implement all of arithmetic, compares and bitwise
    Not all shifts are implemented.
    
    Condition flags are not updated since there's no good type for that yet,
    so compares aren't actually working yet.
    
    All main execution functions seem to be very similar, so maybe attempt
    to find a more high level solution.
    
    Need to split Execution file into several modules.

--- Execution/Execution.fs
+++ Execution/Execution.fs
@@ -1,105 +1,121 @@
-// namespace Visual
-// module Execution
-module Executionion
+module Execution
 open Machine
 
-type RegisterIndex = | R0 | R1 | R2
 
 type APSR = {N:bool; Z:bool; C:bool; V:bool}
 let mutable statusRegister:APSR = {N=false;Z=false;C=false;V=false}
 let updateStatusRegister N Z C V =
     statusRegister <- {N=N; Z=Z; C=C;V=V}
 
-// statusRegister <- updateStatusRegister true false false false
 
 type ConditionSuffix = | EQ | NE | CS | HS | CC | LO | MI | PL | VS | VC | HI
-                        | LS | GE | LT | GT | LE | AL
+                       | LS | GE | LT | GT | LE | AL
 
 type SetBit = | UpdateStatus | IgnoreStatus
 
 // For now, keep the separation, but do we need it in the future?
-type Arithmetic = | ADD | SUB
-type Shift = | LSL
+type Arithmetic = | ADD | SUB | ADC | SBC | RSC
+type Shift = | LSL | LSR | ASR | ROR
 type Compare = | CMP | CMN | TST | TEQ
-
-// type OperationCore = | Arithmetic of ArithmeticOperation | Shift of ShiftOperation
-// The following is simply conveninet in "Instruction" type definition;
-// Issue: 'a can even be int, but is this a problem? Shouldn't be
-// 'a is in place of OperationCore
-type 'a IOperation = 'a * SetBit * ConditionSuffix
+type Bitwise = | AND | EOR | BIC | ORR
 
 type ArithmeticOperation = Arithmetic * SetBit * ConditionSuffix
+type BitwiseOperation = Bitwise * SetBit * ConditionSuffix
 type ShiftOperation = Shift * SetBit * ConditionSuffix
 type CompareOperation = Compare * ConditionSuffix
 
-// Need to decide how to handle the expression for op2
 type RegOperand = RegisterIndex
 type MixedOperand = | Register of RegisterIndex | Literal of int
-type ExecOperand = | MixedOp of MixedOperand | ExprOp of RegOperand * Shift * MixedOperand
+type ExecOperand = | MixedOp of MixedOperand
+                   | ExprOp of RegOperand * Shift * MixedOperand
 
 
-// ADD{S}{cond} dest, op1, op2 {, SHIFT_op #expression}
 type ArithmeticOperandsPattern = RegOperand * RegOperand * ExecOperand
-// Need special case for RRX
-// LSL{S}{cond} dest, op1, op2
+type BitwiseOperandsPattern = ArithmeticOperandsPattern
+// Need special case for RRX shift
 type ShiftOperandsPattern = RegOperand * RegOperand * MixedOperand
-// CMP{cond} op1, op2 {, SHIFT_op #expression}
-type CompareOperandsPattern = RegOperand * MixedOperand
+type CompareOperandsPattern = RegOperand * ExecOperand
 
 
 type IOperands = | ArithmeticOperands of ArithmeticOperandsPattern
-                | ShiftOperands of ShiftOperandsPattern
-// type Instruction = IOperation * IOperands
+                 | ShiftOperands of ShiftOperandsPattern
+                 | CompareOperands of CompareOperandsPattern
+                 | BitwiseOperands of BitwiseOperandsPattern
 
 type Instruction =
     | ArithmeticInstruction of ArithmeticOperation * ArithmeticOperandsPattern
     | ShiftInstruction of ShiftOperation * ShiftOperandsPattern
     | CompareInstruction of CompareOperation * CompareOperandsPattern
+    | BitwiseInstruction of BitwiseOperation * BitwiseOperandsPattern
 
-let mutable registers = Map<RegisterIndex, int> []
-type State = Map<RegisterIndex, int>
-let initialState:State = Map<RegisterIndex, int> [(R1, 5)]
+let bitwiseAnd op1 op2 =
+    op1 &&& op2
+let bitwiseOr op1 op2 =
+    op1 ||| op2
+let bitwiseXor op1 op2 =
+    op1 ^^^ op2
+let bitwiseClear op1 op2 =
+    bitwiseAnd op1 ~~~op2
 
-// let state:State = 
+let leftshift op1 op2 = op1 <<< op2
+let rightshift op1 op2 = op1 >>> op2
 
-let leftshift (op1:int) (op2:int) = op1 <<< op2
+let add a b = a + b
+let addCarry a b =
+    let c = if statusRegister.C then 1 else 0
+    add a c |> add b
 
+let subtract a b = a - b
+let subtractCarry a b =
+    let c = if statusRegister.C then 0 else 1
+    subtract a c |> subtract b
 
-let fetchValue (R:RegisterIndex) = initialState.[R]
+let reverseSubtract a b =
+    subtract b a
+let reverseSubtractCarry a b =
+    subtractCarry b a
 
-// let writeValue (R:RegisterIndex) (value:int) =
-//     (fun (s : State) -> Map.add R value s) |> ignore
-    // registers <- Map.add R value registers
+let compare op1 op2 = subtract op1 op2
+let compareNegated op1 op2 = add op1 op2
+let compareTest op1 op2 = bitwiseAnd op1 op2
+let compareTeq op1 op2 = bitwiseXor op1 op2
 
-// let operandValue = function
-//     | Register x -> fetchValue x
-//     | Literal x -> x
-//     | MixedOp mop -> mixedOperandValue mop
+let getShiftFunction = function
+    | LSL -> leftshift
+    | LSR -> rightshift
+    | ASR | ROR -> leftshift
+
+let getArithmeticFunction = function
+    | ADD -> add
+    | SUB -> subtract
+    | ADC -> addCarry
+    | SBC -> subtractCarry
+    | RSC -> reverseSubtractCarry
+
+let getBitwiseFunction = function
+    | AND -> bitwiseAnd
+    | EOR -> bitwiseXor
+    | BIC -> bitwiseClear
+    | ORR -> bitwiseOr
+
+let getCompareFunction = function
+    | CMP -> compare
+    | CMN -> compareNegated
+    | TST -> compareTest
+    | TEQ -> compareTeq
+
 
 let mixedOperandValue op state =
     match op with
-    | Register R -> registerValue R state
+    | Register R -> State.registerValue R state
     | Literal x -> x
 
-let execOperandValue op state =
+let execOperandValue op (state:State) =
     match op with
-    | MixedOp mop -> mixedOperandValue mop
-    | ExprOp (op2, shift, expr) -> leftshift (registerValue op2 state) (mixedOperandValue expr state)
-
-// let state0:State = Map.ofList []
-// let res = writeValue R0 5
-//           >> writeValue R1 6
-//           >> writeValue R2 1
-
-// printf "%A\n" (res state0)
-// let operation1: ArithmeticOperation = ADD, UpdateStatus, EQ
-// let operation2: ShiftOperation = LSL, IgnoreStatus, NoSuffix
-
-// let operands1 = ArithmeticOperands <| (R0, R1, MixedOp <| Register R2)
-// let operands2 = ShiftOperands <| (R0, R1, Literal 3)
-
-// let instruction1 = ArithmeticInstruction (operation1, (R0, R1, MixedOp <| Register R2))
-// let instruction2 = ShiftInstruction (operation2, (R0, R1, Literal 3))
+    | MixedOp mop -> mixedOperandValue mop state
+    | ExprOp (op2, shift, expr) ->
+        let shiftOp = getShiftFunction shift
+        shiftOp (State.registerValue op2 state) (mixedOperandValue expr state)
 
 let conditionHolds = function
     | AL -> true
@@ -108,30 +124,6 @@ let conditionHolds = function
     | CS | HS -> statusRegister.C
     | _ -> false
 
-// let (|ADD_CSPR|_|) x =
-//     let N = x < 0
-//     let Z = x = 0
-//     let C = x > (1<<<32)
-//     let V = false
-//     (N, Z, C, V)
-// let (|N|Z|C|V|) x =
-    
-//     if x < 0 then true else false
-
-
-// let updateStatus result =
-//     match result with
-//     | 
-    
-let add a b = a + b
-let subtract a b = a - b
-
-let getFunction = function
-    | ADD -> add
-    | SUB -> subtract
-
-// let lookup = Map<Arithmetic, int -> int -> int>
-// let lookup = Map.ofList [(ADD, add0), (SUB, subtract)]
 let executeArithmetic (operation:ArithmeticOperation)
                       (operands:ArithmeticOperandsPattern)
                       (state:State) =
@@ -139,33 +131,71 @@ let executeArithmetic (operation:ArithmeticOperation)
     match conditionHolds cond with
     | true ->
         let dest, op1, op2 = operands
-        let op1Val, op2Val = registerValue op1 state, execOperandValue op2 state
-        let result = getFunction core <| op1Val <| op2Val
-        // result
-        // Map.add dest result state
+        let op1Val = State.registerValue op1 state
+        let op2Val = execOperandValue op2 state
+        let result = getArithmeticFunction core <| op1Val <| op2Val
+        // Need to update CSPR here
+        State.updateRegister dest result state
     | false ->
         state
-    // let dest, op1, op2 = 
-    //     match operands with
-    //     | (dest, op1, op2) -> (dest, op1, op2)
-    // Need to evaluate op2
-    // type ExecOperand = | MixedOp of MixedOperand | ExprOp of Shift * MixedOperand
-    // type MixedOperand = | Register of RegisterIndex | Literal of int
 
-// let executeShift operation operands =
-//     match operation with
-//     | (op, S, cond) -> 1
-//     |> ignore
-//     1
+let executeShift (operation:ShiftOperation)
+                 (operands:ShiftOperandsPattern)
+                 (state:State) =
+    let core, S, cond = operation
+    if conditionHolds cond then
+        let dest, op1, op2 = operands
+        let op1Val = State.registerValue op1 state
+        let op2Val = mixedOperandValue op2 state
+        let result = getShiftFunction core <| op1Val <| op2Val
+        // Need to update CSPR here
+        State.updateRegister dest result state
+    else
+        state
+
+let executeCompare (operation:CompareOperation)
+                   (operands:CompareOperandsPattern)
+                   (state:State) =
+    let core, cond = operation
+    if conditionHolds cond then
+        let op1, op2 = operands
+        let op1Val = State.registerValue op1 state
+        let op2Val = execOperandValue op2 state
+        let result = getCompareFunction core <| op1Val <| op2Val
+        // Need to update CSPR here
+        state
+    else
+        state
+
+let executeBitwise (operation:BitwiseOperation)
+                   (operands:BitwiseOperandsPattern)
+                   (state:State) =
+    let core, S, cond = operation
+    match conditionHolds cond with
+    | true ->
+        let dest, op1, op2 = operands
+        let op1Val = State.registerValue op1 state
+        let op2Val = execOperandValue op2 state
+        let result = getBitwiseFunction core <| op1Val <| op2Val
+        // Need to update CSPR here
+        State.updateRegister dest result state
+    | false ->
+        state
+       
 
 let executeInstruction (state:State) (instruction:Instruction) =
     match instruction with
-    | ArithmeticInstruction (operation, operands) -> executeArithmetic operation operands state
-    | ShiftInstruction (operation, operands) -> state
-    | _ -> state
+    | ArithmeticInstruction (operation, operands) ->
+        executeArithmetic operation operands state
+    | ShiftInstruction (operation, operands) ->
+        executeShift operation operands state
+    | CompareInstruction (operation, operands) ->
+        executeCompare operation operands state
+    | BitwiseInstruction (operation, operands) ->
+        executeBitwise operation operands state
 
-let operation1: ArithmeticOperation = ADD, UpdateStatus, AL
-let instruction = ArithmeticInstruction (operation1, (R0, R1, MixedOp <| Literal 55))
+// let operation1: ArithmeticOperation = ADD, UpdateStatus, AL
+// let instruction = ArithmeticInstruction (operation1, (R0, R1, MixedOp <| Literal 55))
 
-let newState = executeInstruction initialState instruction
+// let newState = executeInstruction initialState instruction
 
