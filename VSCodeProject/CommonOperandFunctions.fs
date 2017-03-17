24676568de5be9fcb4cdcf20848ab775fb68e4ad
Nick <nikita.karandej@gmail.com>
2017-03-17 14:44:24 +0000

    Rewrite execOperandValue; finish conditionHolds
    execOperandValue now returns both value and carry bit.
    conditionHolds has been extended to all condition flags

--- VSCodeProject/CommonOperandFunctions.fs
+++ VSCodeProject/CommonOperandFunctions.fs
@@ -14,21 +14,36 @@ let mixedOperandValue op state =
 
 let execOperandValue op (state:State) =
     match op with
-    | MixedOp mop -> mixedOperandValue mop state
+    | MixedOp mop -> int64 <| mixedOperandValue mop state, 0
     | ExprOp (op2, shift, expr) ->
-        let shiftOp = getShiftFunction shift
-        shiftOp (State.registerValue op2 state) (mixedOperandValue expr state)
+        let op2Val = registerOperandValue op2 state
+        let shiftVal = mixedOperandValue expr state
+        let {body=num; carry=c} = applyShiftFunction shift 0 op2Val shiftVal
+        int64 num, c
 
-let conditionHolds state cond= 
-    let systemValue = match cond with
-                      | AL -> BitValue true
-                      | EQ -> State.statusBitValue Z state
-                      | EQ -> State.statusBitValue Z state// statusRegister.Z
-                      | NE -> State.statusBitValue Z state
-                      | CS | HS -> State.statusBitValue C state
-                      | _ -> failwithf ""
+let conditionHolds state cond = 
+    let getBool state statusBit =
+        let x = State.statusBitValue statusBit state
+        match x with
+        | BitValue b -> b
+        | _ -> failwith "Condition error: systemvalue returned was not bool"
     
-    match systemValue with
-    | BitValue b -> b
-    | _ -> failwithf "conditionHolds error: systemvalue returned was not bool"
+    let getStateFlag = getBool state
+
+    match cond with
+    | AL -> true
+    | EQ -> getStateFlag Z
+    | NE -> not <| getStateFlag Z
+    | CS | HS -> getStateFlag C
+    | CC | LO -> not <| getStateFlag C
+    | MI -> getStateFlag N
+    | PL -> not <| getStateFlag N
+    | VS -> getStateFlag V
+    | VC -> not <| getStateFlag V
+    | HI -> getStateFlag C && not <| getStateFlag Z
+    | LS -> not <| getStateFlag C && getStateFlag Z
+    | GE -> getStateFlag N = getStateFlag V
+    | LT -> getStateFlag N <> getStateFlag V
+    | GT -> (getStateFlag N = getStateFlag V) && not <| getStateFlag Z
+    | LE -> (getStateFlag N <> getStateFlag V) && getStateFlag Z
 
