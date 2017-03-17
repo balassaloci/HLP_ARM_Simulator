5787110bce9a51edc67935378f804fbe66c8fd13
Nick <nikita.karandej@gmail.com>
2017-03-17 17:31:36 +0000

    Add documentation to functions

--- VSCodeProject/CommonOperandFunctions.fs
+++ VSCodeProject/CommonOperandFunctions.fs
@@ -4,14 +4,18 @@ open Machine
 open InstructionsCommonTypes
 open Functions
 
+/// Fetches the numeric value stored in given register
 let registerOperandValue op state =
     State.registerValue op state
 
+/// Extracts numeric value from mixed operand
 let mixedOperandValue op state =
     match op with
     | Register R -> State.registerValue R state
     | Literal x -> x
 
+/// Either extracts numeric value from operand or the value
+/// after shifting the operand.
 let execOperandValue op (state:State) =
     match op with
     | MixedOp mop -> int64 <| mixedOperandValue mop state, 0
@@ -21,12 +25,10 @@ let execOperandValue op (state:State) =
         let {body=num; carry=c} = applyShiftFunction shift 0 op2Val shiftVal
         int64 num, c
 
+/// Returns true if the given condition code is satisfied
 let conditionHolds state cond = 
     let getBool state statusBit =
-        let x = State.statusBitValue statusBit state
-        match x with
-        | BitValue b -> b
-        | _ -> failwith "Condition error: systemvalue returned was not bool"
+        State.statusBitValue statusBit state
     
     let getStateFlag = getBool state
 
