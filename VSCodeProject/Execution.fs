b6c285ae5047958934deea967f1209be63e241da
Lorinc Balassa <lb3314@ic.ac.uk>
2017-03-20 15:23:27 +0000

    Parse line within execute fn

--- VSCodeProject/Execution.fs
+++ VSCodeProject/Execution.fs
@@ -2,11 +2,38 @@ module Execution
 
 open Machine
 open InstructionsCommonTypes
+open ALU
+open Memory
+open Branch
+open CommonParserFunctions
 
 type Instruction =
     | ALUInst of ALU.ALUInstruction
     | MemInst of Memory.MemoryInstruction
     | BrInst of Branch.BranchInstruction
+
+let parseLine (instrS: string) :  (string Option * Instruction) =
+    let parseInstruction (instrS: string) : Instruction =
+        if instrS.Length > 2 then
+            match instrS.[..2] with
+            | "ADD" | "SUB" | "ADC" | "SBC" | "RSC"
+                | "LSL" | "LSR" | "ASR" | "ROR" | "RRX"
+                | "CMP" | "CMN" | "TST" | "TEQ" | "AND"
+                | "EOR" | "BIC" | "ORR" -> ALUInst <| ALUInstruction.parse instrS
+            | "MOV" | "MVN" | "LDR" | "STR" | "LDM"
+                | "STM" | "ADR" -> MemInst <| MemoryInstruction.parse instrS
+        else
+            match instrS with
+            | Prefix "B" _ -> BrInst <| BranchInstruction.parse instrS
+            | _ -> failwithf "Unable to parse instruction"
+    try
+        None, parseInstruction (instrS)
+    with
+    | _ -> match splitInstr instrS with
+           | l, i -> Some (l), parseInstruction i
+
+let parseAll (txt: string) =
+    1
 
 
 
